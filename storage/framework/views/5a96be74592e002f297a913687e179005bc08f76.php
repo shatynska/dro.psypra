d953233adb5f9acf3c32c66ca78134642231c8c5
niftywebsite <mail@getanifty.website>
2022-12-29 16:07:25 +0200

    test npm build in GHA

--- storage/framework/views/5a96be74592e002f297a913687e179005bc08f76.php
+++ storage/framework/views/5a96be74592e002f297a913687e179005bc08f76.php
@@ -1,3 +1,3 @@
 <footer class="container">
-    <p>copyright@2022, All Rights reserved</p>
+    <p>copyright@2023, All Rights reserved</p>
 </footer><?php /**PATH /var/www/dro.psypra.com/current/resources/views/partials/_footer.blade.php ENDPATH**/ ?>
