e1c93104ab7a09b017e50c9e03bb9738240ac24f
Helen Shatynska <helen.shatynska@gmail.com>
2024-01-29 20:13:57 +0200

    Clean up initial files

---
+++ src/app/page.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import Home from '../app/page';
+
+test('Home', () => {
+  render(<Home />);
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+      name: 'Психологи, психотерапевти та психіатри Дрогобиччини',
+    }),
+  ).toBeDefined();
+});
