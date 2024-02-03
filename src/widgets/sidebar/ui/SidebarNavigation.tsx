830b779b6c43072d2598ccdf421e621451b404e1
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-03 12:18:45 +0200

    Add SidebarNavigation component

---
+++ src/widgets/sidebar/ui/SidebarNavigation.tsx
@@ -0,0 +1,86 @@
+import { documents, sections, specialists } from '~/shared/api/mock';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionListItem,
+  AccordionTopListItem,
+  AccordionTrigger,
+  Logomark,
+} from '~/shared/ui';
+
+export function SidebarNavigation() {
+  return (
+    <Accordion type="single" collapsible className="relative top-12">
+      <AccordionItem value="item-1">
+        <AccordionTopListItem
+          key="{specialist.name}"
+          title="головна"
+          href="/"
+        />
+      </AccordionItem>
+
+      <AccordionItem value="item-2">
+        <AccordionTrigger>фахівці</AccordionTrigger>
+        <AccordionContent>
+          <ul>
+            {specialists.map((specialist) => (
+              <AccordionListItem
+                key={specialist.name}
+                title={specialist.name}
+                href={specialist.href}
+              >
+                {specialist.description}
+              </AccordionListItem>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-3">
+        <AccordionTrigger>про нас</AccordionTrigger>
+        <AccordionContent>
+          <ul>
+            <AccordionListItem href="/go">
+              <div className="flex flex-col content-center items-center justify-end rounded-md bg-gradient-to-b from-muted/20 to-muted p-6 text-foreground no-underline focus:bg-muted focus:text-accent-foreground focus:outline-none">
+                <Logomark size="sm" />
+                <div className="mb-2 mt-4 text-center text-lg font-medium">
+                  Психотерапевти Дрогобиччини
+                </div>
+                <p className="text-center text-sm text-muted-foreground">
+                  громадська організація
+                </p>
+              </div>
+            </AccordionListItem>
+            {documents.map((document) => (
+              <AccordionListItem
+                key={document.title}
+                title={document.title}
+                href={document.href}
+              >
+                {document.description}
+              </AccordionListItem>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+
+      <AccordionItem value="item-4">
+        <AccordionTrigger>інформація</AccordionTrigger>
+        <AccordionContent>
+          <ul>
+            {sections.map((section) => (
+              <AccordionListItem
+                key={section.title}
+                title={section.title}
+                href={section.href}
+              >
+                {section.description}
+              </AccordionListItem>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
