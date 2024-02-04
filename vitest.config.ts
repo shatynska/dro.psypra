4ed737942f6e85aa7f1cf3900f37bde6815d80a8
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-05 00:17:47 +0200

    Add aliases to vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path';
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
@@ -5,5 +7,20 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: [
+      { find: '~/app', replacement: resolve(__dirname, './src/app') },
+      { find: '~/widgets', replacement: resolve(__dirname, './src/widgets') },
+      {
+        find: '~/features',
+        replacement: resolve(__dirname, './src/features'),
+      },
+      {
+        find: '~/entities',
+        replacement: resolve(__dirname, './src/entities'),
+      },
+      { find: '~/shared', replacement: resolve(__dirname, './src/shared') },
+    ],
   },
 });
