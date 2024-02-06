9d9ef99f6b471b701ba555fb7c890abdf12ca631
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-06 16:55:03 +0200

    Decrease top css value in SheetTrigger & Close buttons

--- src/shared/ui/Sheet.tsx
+++ src/shared/ui/Sheet.tsx
@@ -67,7 +67,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="fixed right-2 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary sm:right-4">
+      <SheetPrimitive.Close className="fixed right-2 top-2 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary sm:right-4">
         <Button size={'icon'} variant={'outline'} asChild>
           <X />
         </Button>
