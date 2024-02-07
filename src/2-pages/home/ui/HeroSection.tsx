a140861f2938e476993960777abaa59f2f190d37
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-07 23:10:51 +0200

    Reduce the size of Hero image & title

--- src/2-pages/home/ui/HeroSection.tsx
+++ src/2-pages/home/ui/HeroSection.tsx
@@ -6,9 +6,9 @@ import HeroInfo from './HeroInfo';
 
 export function HeroSection() {
   return (
-    <div className="relative -top-16 h-[736px] w-full bg-gradient-to-b from-red-gradient-dark to-red-gradient-light bg-[length:100%_720px] bg-top bg-no-repeat pt-16 lg:h-[832px] lg:bg-gradient-to-t lg:pt-16">
-      <div className="absolute h-[720px] w-full bg-[url('/images/hero-purple-fon.svg')] bg-[left_-9.5rem_top_-2rem] bg-no-repeat sm:bg-[left_-2rem_top_-6rem] lg:h-[840px] lg:bg-[center_top_2rem]" />
-      <div className="absolute top-[560px] h-60 w-full bg-gradient-to-t from-blue-gradient-dark to-blue-gradient-light lg:hidden" />
+    <div className="relative -top-16 h-[684px] w-full bg-gradient-to-b from-red-gradient-dark to-red-gradient-light bg-[length:100%_720px] bg-top bg-no-repeat pt-16 sm:h-[752px] lg:h-[832px] lg:bg-gradient-to-t lg:pt-16">
+      <div className="absolute h-[620px] w-full bg-[url('/images/hero-purple-fon.svg')] bg-[left_-9.5rem_top_-2rem] bg-no-repeat sm:bg-[left_-2rem_top_-6rem] lg:h-[840px] lg:bg-[center_top_2rem]" />
+      <div className="absolute top-[492px] h-64 w-full bg-gradient-to-t from-red-gradient-dark to-red-gradient-light sm:top-[560px] lg:hidden" />
 
       <section className="flex flex-col gap-y-36 text-background sm:gap-y-24 lg:flex-row lg:content-between lg:justify-center lg:pt-36">
         <div className="relative flex w-full flex-col items-center lg:w-auto">
@@ -21,9 +21,9 @@ export function HeroSection() {
             />
           </div>
           <div className="relative">
-            <Logotype className="absolute left-48 top-16 sm:left-56 sm:text-lg lg:text-2xl" />
+            <Logotype className="absolute left-[148px] top-12 text-sm sm:left-56 sm:top-16 sm:text-lg lg:text-2xl" />
             <div className="relative flex flex-col lg:flex-row lg:items-end">
-              <div className="relative h-[216px] w-[323px] sm:h-[258px] sm:w-[388px] lg:w-[358px]">
+              <div className="relative h-[172px] w-[258px] sm:h-[258px] sm:w-[388px] lg:w-[358px]">
                 <Image
                   src={'/images/dro.svg'}
                   alt="ДРО"
@@ -32,7 +32,7 @@ export function HeroSection() {
                 />
               </div>
               <div>
-                <div className="relative text-6xl font-extrabold sm:text-7xl">
+                <div className="relative text-5xl font-extrabold sm:text-7xl">
                   гобиччини
                 </div>
               </div>
