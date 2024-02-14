05b9d0743c5a8f0f8e5fb18496009de77fb2913e
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-14 23:51:02 +0200

    Use Carousel in SpecialtiesSection & update related mock

--- src/2-pages/home/ui/SpecialtiesSection.tsx
+++ src/2-pages/home/ui/SpecialtiesSection.tsx
@@ -1,33 +1,60 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import {
   specialtiesSectionHeadings as headings,
   specialtiesSectionPosts as posts,
 } from '~/shared/api/mock';
+import { cn } from '~/shared/lib';
 import { Card, CardContent, CardHeader, CardTitle } from '~/shared/ui/Card';
+import { CarouselApi, CarouselItem } from '~/shared/ui/Carousel';
+import { CarouselWrapper } from '~/shared/ui/CarouselWrapper';
 import { Section } from '~/shared/ui/Section';
 
 export function SpecialtiesSection() {
-  const top = new Map([
-    [0, 'mt-20'],
-    [1, 'mt-10'],
-    [2, 'mt-0'],
-  ]);
+  const [api, setApi] = useState<CarouselApi>();
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    api.on('select', () => {
+      setSelectedIndex(api.selectedScrollSnap());
+    });
+  }, [api]);
 
   return (
-    <Section variant={'dark'} headings={headings}>
-      <div className="flex flex-col gap-24 lg:flex-row">
-        {posts.map((post, index) => {
-          return (
-            <Card key={post.title} className={top.get(index)}>
+    <Section variant="dark" headings={headings}>
+      <CarouselWrapper
+        variant={posts.length > 3 ? 'upward' : 'default'}
+        setApi={setApi}
+      >
+        {posts.map((post, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <Card
+              key={post.title}
+              className={cn(
+                selectedIndex >= index
+                  ? 'lg:mt-20'
+                  : selectedIndex === index - 1
+                    ? 'lg:mt-10'
+                    : 'lg:mt-0',
+              )}
+            >
               <CardHeader>
                 <CardTitle>{post.title}</CardTitle>
               </CardHeader>
-              <CardContent className="line-clamp-10">
+              <CardContent className="line-clamp-10 text-justify md:hyphens-none md:text-left lg:line-clamp-7">
                 {post.content}
               </CardContent>
             </Card>
-          );
-        })}
-      </div>
+          </CarouselItem>
+        ))}
+      </CarouselWrapper>
     </Section>
   );
 }
