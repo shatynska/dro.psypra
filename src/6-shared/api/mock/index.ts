9c9ba7ed898a8056a8b6655444706bfa981830b6
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-15 22:02:14 +0200

    Refactor api/mock data for navigation

--- src/6-shared/api/mock/index.ts
+++ src/6-shared/api/mock/index.ts
@@ -1,9 +1,7 @@
-export { documents } from './documents.mock';
 export { members, membersHeadings } from './members.mock';
+export { documents, sections, specialists } from './navigation';
 export { questionsSectionHeadings } from './questions-section.mock';
-export { sections } from './sections.mock';
 export { specialistsSectionHeadings } from './specialists-section.mock';
-export { specialists } from './specialists.mock';
 export {
   specialtiesSectionHeadings,
   specialtiesSectionPosts,
