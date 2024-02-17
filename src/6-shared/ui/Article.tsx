bf3c8e4e809a028d6187d9bdfe5a40d66710a172
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-17 09:44:59 +0200

    Refine style rules for Article

--- src/6-shared/ui/Article.tsx
+++ src/6-shared/ui/Article.tsx
@@ -6,7 +6,7 @@ export function Article({ className, children, ...props }: Props) {
   return (
     <article
       className={cn(
-        '[&_h2]:py-4 [&_h2]:text-2xl [&_h2]:font-bold [&_h3]:py-2 [&_h3]:text-lg [&_h3]:font-bold [&_p]:mb-4',
+        'mb-8 max-w-192 pl-6 lg:pl-12 [&_h2]:py-4 [&_h2]:pl-8 [&_h2]:text-2xl [&_h2]:font-bold lg:[&_h2]:pl-12 [&_h3]:py-2 [&_h3]:text-lg [&_h3]:font-bold [&_p]:mb-4',
         className,
       )}
       {...props}
