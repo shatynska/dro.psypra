0f7628a12b87f9ae7f5902954b1d6ef65630b550
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-21 08:54:35 +0200

    Remove Aspects Sections and move section back to Home

--- src/2-pages/home/ui/Home.tsx
+++ src/2-pages/home/ui/Home.tsx
@@ -1,11 +1,45 @@
-import { AspectsSections } from './AspectsSections';
+import { SectionGroup } from '~/shared/ui/SectionGroup';
+
+import { ApproachesSection } from './ApproachesSection';
+import { ContactsSection } from './ContactsSection';
+import { CostsSection } from './CostsSection';
+import { FormsSection } from './FormsSection';
 import { HeroSection } from './HeroSection';
+import { LocationsSection } from './LocationsSection';
+import { QuestionsSection } from './QuestionsSection';
+import { SpecialistsSection } from './SpecialistsSection';
+import { SpecialtiesSection } from './SpecialtiesSection';
+import { TermsSection } from './TermsSection';
+import { ThemesSection } from './ThemesSection';
+import { TimesSection } from './TimesSection';
 
 export function Home() {
   return (
     <>
       <HeroSection />
-      <AspectsSections />
+      <div>
+        <QuestionsSection />
+      </div>
+      <SectionGroup>
+        <SpecialtiesSection />
+        <SpecialistsSection />
+      </SectionGroup>
+      <SectionGroup variant="red">
+        <FormsSection />
+        <ThemesSection />
+      </SectionGroup>
+      <SectionGroup>
+        <TermsSection />
+        <ApproachesSection />
+      </SectionGroup>
+      <SectionGroup variant="red">
+        <CostsSection />
+        <LocationsSection />
+      </SectionGroup>
+      <SectionGroup>
+        <TimesSection />
+        <ContactsSection />
+      </SectionGroup>
     </>
   );
 }
