6b98472f3df413465913d29b9c18c0cafd4353f3
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-21 15:21:49 +0200

    Add title parameter to Article

--- src/6-shared/ui/Article.tsx
+++ src/6-shared/ui/Article.tsx
@@ -2,15 +2,16 @@ import { cn } from '../lib';
 
 type Props = React.HTMLAttributes<HTMLElement>;
 
-export function Article({ className, children, ...props }: Props) {
+export function Article({ title, className, children, ...props }: Props) {
   return (
     <article
       className={cn(
-        'mb-8 max-w-192 pl-6 lg:pl-12 [&_h2]:py-4 [&_h2]:pl-8 [&_h2]:text-2xl [&_h2]:font-bold lg:[&_h2]:pl-12 [&_h3]:py-2 [&_h3]:text-lg [&_h3]:font-bold [&_p]:mb-4',
+        'mb-8 max-w-192 pl-6 lg:pl-12 [&_h3]:py-2 [&_h3]:text-lg [&_h3]:font-bold [&_p]:mb-4',
         className,
       )}
       {...props}
     >
+      <h2 className="py-4 pl-8 text-2xl font-bold lg:pl-12">{title}</h2>
       {children}
     </article>
   );
