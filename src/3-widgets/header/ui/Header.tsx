40f265a2edd71928a45e110f11edaec6759c34cf
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-26 23:53:35 +0200

    Set Header as sticky since medium screens & change height

--- src/3-widgets/header/ui/Header.tsx
+++ src/3-widgets/header/ui/Header.tsx
@@ -8,9 +8,9 @@ export function Header() {
   return (
     <header
       role="banner"
-      className="absolute left-0 top-0 z-10 flex h-32 w-full bg-gradient-to-b from-blue-gradient-dark to-blue-gradient-light bg-[length:100%_6rem] pt-4 text-background lg:bg-[url('/images/header-fon.svg')] lg:bg-[length:auto]"
+      className="relative left-0 top-0 z-10 flex h-[5.5rem] w-full bg-gradient-to-b from-blue-gradient-dark to-blue-gradient-light bg-[length:100%_6rem] pt-4 text-background md:sticky lg:h-28 lg:bg-[url('/images/header-fon.svg')] lg:bg-[length:auto]"
     >
-      <div className="flex h-14 items-center justify-between">
+      <div className="flex items-center justify-between">
         <Link href="/">
           <Logo variant="header" />
         </Link>
