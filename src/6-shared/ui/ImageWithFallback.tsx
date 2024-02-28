2319d63458f6bb578f1b1e7d1fa14f2a924123da
Helen Shatynska <helen.shatynska@gmail.com>
2024-02-28 19:41:29 +0200

    Add ImageWithFallback shared component

---
+++ src/6-shared/ui/ImageWithFallback.tsx
@@ -0,0 +1,24 @@
+import Image, { ImageProps } from 'next/image';
+import { memo } from 'react';
+
+type Props = ImageProps & {
+  alt: string;
+  srcFallback: string;
+};
+
+const ImageWithFallback = ({ src, srcFallback, alt, ...props }: Props) => {
+  return (
+    <Image
+      src={src ? src : srcFallback}
+      alt={alt}
+      onError={(event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        const target = event.target as HTMLImageElement;
+        target.id = srcFallback;
+        target.srcset = srcFallback;
+      }}
+      {...props}
+    />
+  );
+};
+
+export default memo(ImageWithFallback);
