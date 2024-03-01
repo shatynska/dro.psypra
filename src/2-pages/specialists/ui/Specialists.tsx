2025eb9af4fc1661afc9ca24d3963cc055563f7e
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-01 18:58:34 +0200

    Add SpecialistCardList to Specialist page & widget

--- src/2-pages/specialists/ui/Specialists.tsx
+++ src/2-pages/specialists/ui/Specialists.tsx
@@ -1,17 +1,17 @@
 import { useGetSpecialistsControllerExecuteSuspense } from '~/shared/api';
 import { Section } from '~/shared/ui/Section';
 
+import { SpecialistCardList } from '~/entities/specialist';
+
 export function Specialists() {
   const { headings, parentLink, items } =
     useGetSpecialistsControllerExecuteSuspense();
 
   return (
     <div>
-      <Section
-        headings={headings}
-        parentLink={parentLink}
-        type="main"
-      ></Section>
+      <Section headings={headings} parentLink={parentLink} type="main">
+        <SpecialistCardList items={items} direction="up" />
+      </Section>
     </div>
   );
 }
--- src/3-widgets/specialists/ui/Specialists.tsx
+++ src/3-widgets/specialists/ui/Specialists.tsx
@@ -1,9 +1,15 @@
 import { useGetSpecialistsControllerExecuteSuspense } from '~/shared/api';
 import { Section } from '~/shared/ui/Section';
 
+import { SpecialistCardList } from '~/entities/specialist';
+
 export function Specialists() {
   const { headings, href, items } =
     useGetSpecialistsControllerExecuteSuspense();
 
-  return <Section headings={headings}></Section>;
+  return (
+    <Section headings={headings} href={href}>
+      <SpecialistCardList items={items} />
+    </Section>
+  );
 }
