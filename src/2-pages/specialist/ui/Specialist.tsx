485482a9ade8657f37b30f1026e2cb8c99cb787b
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-05 23:43:41 +0200

    Add specialist sections to Specialist component

--- src/2-pages/specialist/ui/Specialist.tsx
+++ src/2-pages/specialist/ui/Specialist.tsx
@@ -1,21 +1,44 @@
-import { useGetSpecialistControllerExecuteSuspense } from '~/shared/api';
-import { Section } from '~/shared/ui/Section';
+import { SectionGroup } from '~/shared/ui/Section';
 
-type Props = {
+import { Posts } from '~/widgets/posts';
+
+import {
+  SpecialistApproachSection,
+  SpecialistBriefSection,
+  SpecialistContactFormSection,
+  SpecialistExperienceSection,
+  SpecialistOverviewSection,
+  SpecialistPersonalitySection,
+  SpecialistScheduleSection,
+  SpecialistThemesSection,
+} from './sections';
+
+export type SpecialistProps = {
   alias: string;
 };
 
-export function Specialist({ alias }: Props) {
-  const { headings, parentLink } =
-    useGetSpecialistControllerExecuteSuspense(alias);
-
+export function Specialist({ alias }: SpecialistProps) {
   return (
-    <div>
-      <Section
-        headings={headings}
-        parentLink={parentLink}
-        type="main"
-      ></Section>
-    </div>
+    <>
+      <div>
+        <SpecialistOverviewSection alias={alias} />
+      </div>
+      <SectionGroup>
+        <SpecialistBriefSection alias={alias} />
+        <SpecialistExperienceSection alias={alias} />
+      </SectionGroup>
+      <SectionGroup>
+        <SpecialistThemesSection alias={alias} />
+        <SpecialistApproachSection alias={alias} />
+      </SectionGroup>
+      <SectionGroup>
+        <SpecialistScheduleSection alias={alias} />
+        <SpecialistPersonalitySection alias={alias} />
+      </SectionGroup>
+      <SectionGroup>
+        <Posts specialist={alias} />
+        <SpecialistContactFormSection alias={alias} />
+      </SectionGroup>
+    </>
   );
 }
