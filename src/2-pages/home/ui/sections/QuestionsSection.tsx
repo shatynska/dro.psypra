456771cfc4974caabecfce9cd5eef38554d3989a
Helen Shatynska <helen.shatynska@gmail.com>
2024-03-14 11:00:01 +0200

    Use apiClient instead of mock data in QuestionsSection

--- src/2-pages/home/ui/sections/QuestionsSection.tsx
+++ src/2-pages/home/ui/sections/QuestionsSection.tsx
@@ -1,17 +1,21 @@
 import Link from 'next/link';
 
-import { useGetQuestionsControllerExecuteSuspense } from '~/shared/api';
+import { apiClient } from '~/shared/api';
 import { Section } from '~/shared/ui/Section';
 
-export function QuestionsSection() {
-  const { headings, items } = useGetQuestionsControllerExecuteSuspense();
+export async function QuestionsSection() {
+  const { data, error } = await apiClient.GET('/api/pages/home/questions');
+
+  if (error) return;
+
+  const { content, header } = data;
 
   const breaks = new Set([0, 2, 5, 7]);
 
   return (
-    <Section id="questions" headings={headings} height="fixed">
+    <Section id="questions" headings={header.headings} height="fixed">
       <ul className="flex w-full flex-col justify-center gap-x-24 whitespace-nowrap pl-12 sm:pl-16 lg:flex-row lg:flex-wrap lg:items-center lg:gap-y-4 lg:pl-0 lg:text-xl lg:font-bold [&>div]:h-0 [&>div]:basis-full [&_li]:pb-2">
-        {items.map((question, index) => (
+        {content.items.map((question, index) => (
           <>
             <li key={index}>
               <Link href={question.href}>{question.title}</Link>
