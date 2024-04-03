aa89a49896872b67c08861a28ea9cd73097241f1
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-03 23:48:31 +0300

    Update DimensionItemCard

--- src/5-entities/dimension-item/ui/DimensionItemCard.tsx
+++ src/5-entities/dimension-item/ui/DimensionItemCard.tsx
@@ -1,27 +1,32 @@
 import parse from 'html-react-parser';
 import DOMPurify from 'isomorphic-dompurify';
 import Link from 'next/link';
+import { AnchorHTMLAttributes } from 'react';
 
-import { DimensionItem } from '~/shared/api';
+import { cn } from '~/shared/lib';
 import { Card, CardContent, CardHeader, CardTitle } from '~/shared/ui/Card';
 
-type Props = {
-  item: DimensionItem;
-  parentAlias: string;
+type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  item: {
+    title: string;
+    href: string;
+    description?: string;
+  };
 };
 
-export function DimensionItemCard({ item, parentAlias }: Props) {
+export function DimensionItemCard({ item, className, ...props }: Props) {
   return (
     <Link
-      href={`/${parentAlias}/${item.alias}`}
-      className="relative flex justify-center"
+      href={item.href}
+      className={cn('relative flex justify-center', className)}
+      {...props}
     >
       <Card className="md:max-w-80">
         <CardHeader>
-          <CardTitle>{item.headings.primary}</CardTitle>
+          <CardTitle>{item.title}</CardTitle>
         </CardHeader>
         <CardContent className="line-clamp-12 text-justify md:line-clamp-10 md:hyphens-none md:text-left">
-          {item.content && parse(DOMPurify.sanitize(item.content))}
+          {item.description && parse(DOMPurify.sanitize(item.description))}
         </CardContent>
       </Card>
     </Link>
