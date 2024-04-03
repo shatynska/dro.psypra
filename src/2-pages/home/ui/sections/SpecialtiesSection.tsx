14d1f2f5736bc5ec62629d155ed447d915483f57
Helen Shatynska <helen.shatynska@gmail.com>
2024-04-04 00:06:04 +0300

    Replace mock endpoints with real in SpecialtiesSection

--- src/2-pages/home/ui/sections/SpecialtiesSection.tsx
+++ src/2-pages/home/ui/sections/SpecialtiesSection.tsx
@@ -1,22 +1,34 @@
-import { useGetSpecialtiesControllerExecuteSuspense } from '~/shared/api';
+import { apiClient } from '~/shared/api';
 import { CarouselItem, CarouselWrapper } from '~/shared/ui/Carousel';
 import { Section } from '~/shared/ui/Section';
 
 import { DimensionItemCard } from '~/entities/dimension-item';
 
-export function SpecialtiesSection() {
-  const { headings, href, items } =
-    useGetSpecialtiesControllerExecuteSuspense();
+export async function SpecialtiesSection() {
+  const { data, error } = await apiClient.GET('/api/pages/home/{dimension}', {
+    params: {
+      path: { dimension: 'specialties' },
+    },
+  });
+
+  if (error) return;
+
+  const { header, content } = data;
 
   return (
-    <Section headings={headings} href={href} background="dark" height="fixed">
+    <Section
+      headings={header.headings}
+      href={header.href}
+      background="dark"
+      height="fixed"
+    >
       <CarouselWrapper
-        variant={items.length > 3 ? 'upward' : 'default'}
+        variant={content.items.length > 3 ? 'upward' : 'default'}
         maxSlidesInView={3}
       >
-        {items.map((specialty, index) => (
+        {content.items.map((specialty, index) => (
           <CarouselItem index={index} key={specialty.alias}>
-            <DimensionItemCard item={specialty} parentAlias="specialties" />
+            <DimensionItemCard item={specialty} />
           </CarouselItem>
         ))}
       </CarouselWrapper>
